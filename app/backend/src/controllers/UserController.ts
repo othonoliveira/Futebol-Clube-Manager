1ebaccb3388e2924ad852340278656307bb060a2
Othon Oliveira <othonoliveira08@gmail.com>
2023-05-08 23:13:00 -0300

    Requisito 10

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -9,7 +9,20 @@ export default class UserControler {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
 
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (!emailRegex.test(email)) {
+      return res.status(401).json({
+        message: 'Invalid email or password',
+      });
+    }
+    if (password.length < 6) {
+      return res.status(401).json({
+        message: 'Invalid email or password',
+      });
+    }
+
     const { status, message }:ILoginReturn = await this.Service.login({ email, password });
-    return res.status(status).json({ message });
+    return res.status(status).json({ token: message });
   };
 }
--- app/backend/src/services/UserServices.ts
+++ app/backend/src/services/UserServices.ts
@@ -1,3 +1,4 @@
+import { compareSync } from 'bcryptjs';
 import createToken from '../auth/createToken';
 import { ILogin, ILoginReturn } from '../interfaces/UserInterfaces';
 import Users from '../database/models/UserModel';
@@ -6,7 +7,15 @@ export default class LoginService {
   private token = '';
   private user: Users | null = new Users();
 
-  async login({ email }: ILogin): Promise<ILoginReturn> {
+  async login({ email, password }: ILogin): Promise<ILoginReturn> {
+    const user = await Users.findOne({ where: { email } });
+
+    if (!user) return { status: 401, message: 'Invalid email or password' };
+
+    if (!compareSync(password, user.password)) {
+      return { status: 401, message: 'Invalid email or password' };
+    }
+
     this.token = createToken(6000, { email });
     return { status: 200, message: this.token };
   }
